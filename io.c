74f364d50099fbaeab735142ded2483feed8c6ab
Adam <adamharald440@gmail.com>
2021-05-01 18:58:42 +0200

    io: Added return values to get_user_command
    Also added function to save a deck of cards, contained in a linked list,
    to a file

--- io.c
+++ io.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <regex.h>
 
 #include "io.h"
 #include "game.h"
+#include "gui.h"
+
+char moved_card[2];
+
+char source_column[2];
+
+char destination_column[2];
+
+char argument[64];
 
 linked_list *load_from_file(char *filepath) {
-    if (validate_file(filepath) != 0) {
-        return NULL;
-    }
-
     FILE *file = fopen(filepath, "r");
 
     linked_list *list = malloc(sizeof(linked_list));
@@ -37,9 +40,12 @@ linked_list *load_from_file(char *filepath) {
 
 int validate_file(char *filepath) {
 
+    static char output_buffer[512];
+
     FILE *file = fopen(filepath, "r");
     if (!file) {
-        printf("File '%s' not found\n", filepath);
+        sprintf(output_buffer, "File '%s' not found", filepath);
+        set_message(output_buffer);
         return FILE_NOT_FOUND;
     }
 
@@ -59,15 +65,19 @@ int validate_file(char *filepath) {
 
     while (fscanf(file, "%[^\r\n] ", buffer) != EOF) {
         if (strlen(buffer) != 2) {
-            printf("Unknown card format '%s' on line %d: Valid format is [name-char][suit-char] e.g. TH for ten of hearts\n",
-                   buffer, line_number);
+            sprintf(output_buffer,
+                    "Unknown card format '%s' on line %d: Valid format is [name-char][suit-char] e.g. TH for ten of hearts",
+                    buffer, line_number);
+            set_message(output_buffer);
             fclose(file);
             return UNKNOWN_FORMAT;
         }
 
         card_value = get_card_value(buffer[0]);
         if (card_value == -1) {
-            printf("Unknown card name '%c' on line %d: Valid names are A, 2-9, T, J, Q, K\n", buffer[0], line_number);
+            sprintf(output_buffer, "Unknown card name '%c' on line %d: Valid names are A, 2-9, T, J, Q, K", buffer[0],
+                    line_number);
+            set_message(output_buffer);
             fclose(file);
             return UNKNOWN_NAME;
         }
@@ -86,13 +96,16 @@ int validate_file(char *filepath) {
                 ++suit_count[3];
                 break;
             default:
-                printf("Unknown card suit '%c' on line %d: Valid suits are C, D, H, S\n", buffer[1], line_number);
+                sprintf(output_buffer, "Unknown card suit '%c' on line %d: Valid suits are C, D, H, S", buffer[1],
+                        line_number);
+                set_message(output_buffer);
                 fclose(file);
                 return UNKNOWN_SUIT;
         }
 
         if (++card_count[card_value - 1] > 4) {
-            printf("Too many '%c' cards. Excess is on line %d\n", buffer[0], line_number);
+            sprintf(output_buffer, "Too many '%c' cards. Excess is on line %d", buffer[0], line_number);
+            set_message(output_buffer);
             fclose(file);
             return WRONG_NAME_CARD_COUNT;
         }
@@ -120,8 +133,8 @@ int validate_file(char *filepath) {
                 default:
                     suit = "UNKNOWN";
             }
-            printf("Wrong number of %s cards: Should be 13 not %d\n", suit, suit_count[i]);
-
+            sprintf(output_buffer, "Wrong number of %s cards: Should be 13 not %d", suit, suit_count[i]);
+            set_message(output_buffer);
             fclose(file);
             return WRONG_SUIT_CARD_COUNT;
         }
@@ -133,47 +146,70 @@ int validate_file(char *filepath) {
 
 int get_user_input() {
 
-    char buffer[128];
+    static char buffer[128];
 
-    scanf("%s", buffer);
-
+    fgets(buffer, 128, stdin);
     size_t command_length = strlen(buffer);
+    buffer[--command_length] = '\0';
+
+    set_last_command(buffer);
+
     if (command_length == 1) {
 
         if (buffer[0] == 'Q') {
             // Quit current game
+            return QUIT_GAME;
 
         } else if (buffer[0] == 'P') {
             // Start game with current deck
+            return PLAY;
 
         } else {
             // Unknown input
+            return WRONG_INPUT_FORMAT;
         }
 
     } else if (command_length == 2) {
 
         if (buffer[0] == 'Q' && buffer[1] == 'Q') {
             // Quit program
+            return QUIT_PROGRAM;
 
         } else if (buffer[0] == 'L' && buffer[1] == 'D') {
             // Load a deck from file
             // Here, no file is specified, so a default deck should be loaded
+            argument[0] = '\0';
+            return LOAD_FILE;
 
-        } else if (buffer[0] == 'S' && buffer[1] == 'W') {
-            // Show all cards
+        } else if (buffer[0] == 'S') {
 
-        } else if (buffer[0] == 'S' && buffer[1] == 'I') {
-            // Shuffle split
+            if (buffer[1] == 'W') {
+                // Show all cards
+                return SHOW_CARDS;
 
-        } else if (buffer[0] == 'S' && buffer[1] == 'R') {
-            // Shuffle random
+            } else if (buffer[1] == 'I') {
+                // Shuffle split
+                // Here, split is not specified, so a random number should be chosen
+                argument[0] = '\0';
+                return SHUFFLE_SPLIT;
 
-        } else if (buffer[0] == 'S' && buffer[1] == 'D') {
-            // Save cards to a file
-            // Here, no filename is specified, so cards should be saved to default filename 'cards.txt'
+            } else if (buffer[1] == 'R') {
+                // Shuffle random
+                return SHUFFLE_RANDOM;
 
+            } else if (buffer[1] == 'D') {
+                // Save cards to a file
+                // Here, filename is not specified, so cards should be saved to default filename 'cards.txt'
+                argument[0] = '\0';
+
+                return SAVE_DECK;
+
+            } else {
+                return WRONG_INPUT_FORMAT;
+            }
         } else {
             // Unknown input
+            return WRONG_INPUT_FORMAT;
         }
 
     } else {
@@ -181,17 +217,105 @@ int get_user_input() {
         if (buffer[0] == 'L' && buffer[1] == 'D' && buffer[2] == ' ') {
             // Load a deck from file
             // Here, a file might be specified (If there is something after the space)
+            if (command_length > 3) {
+                strncpy(argument, buffer + 3, command_length - 3);
+
+            } else {
+                argument[0] = '\0';
+            }
+
+            return LOAD_FILE;
 
         } else if (buffer[0] == 'S' && buffer[1] == 'D' && buffer[2] == ' ') {
             // Save cards to a file
             // Here, a filename might be specified (If there is something after the space)
+            if (command_length > 3) {
+                strncpy(argument, buffer + 3, command_length - 3);
+
+            } else {
+                argument[0] = '\0';
+            }
+
+            return SAVE_DECK;
+
+        } else if (buffer[0] == 'S' && buffer[1] == 'I' && buffer[2] == ' ') {
+            // Shuffle split
+            // Here, split might be specified
+            if (command_length > 3) {
+                strncpy(argument, buffer + 3, command_length - 3);
+
+            } else {
+                argument[0] = '\0';
+            }
+
+            return SHUFFLE_SPLIT;
 
         } else {
             // Game moves
             // REGEX string if we can use it: /^[\w]{2}(:[\w]{2})?->[\w]{2}$/gm
-        }
 
+            if (command_length == 6) {
+                if (buffer[2] != '-' || buffer[3] != '>') {
+                    return WRONG_INPUT_FORMAT;
+                }
+                source_column[0] = buffer[0];
+                source_column[1] = buffer[1];
+
+                destination_column[0] = buffer[5];
+                destination_column[1] = buffer[6];
+
+                moved_card[0] = '\0';
+
+                return MOVE_CARD;
+            } else if (command_length == 9) {
+                if (buffer[2] != ':' || buffer[5] != '-' || buffer[6] != '>') {
+                    return WRONG_INPUT_FORMAT;
+                }
+                source_column[0] = buffer[0];
+                source_column[1] = buffer[1];
+
+                moved_card[0] = buffer[3];
+                moved_card[1] = buffer[4];
+
+                destination_column[0] = buffer[7];
+                destination_column[1] = buffer[8];
+
+                return MOVE_CARD;
+
+            } else {
+                return WRONG_INPUT_FORMAT;
+            }
+        }
+    }
+}
+
+void save_deck_to_file(linked_list *list, char *filepath) {
+    FILE *file = fopen(filepath, "w");
+
+    node *cursor = list->head;
+    while (cursor != list->dummy) {
+        fputc(cursor->card->name, file);
+        fputc(cursor->card->suit, file);
+        fputc('\n', file);
+
+        cursor = cursor->next;
     }
 
-    return 0;
+    fclose(file);
+}
+
+char *get_moved_card() {
+    return moved_card;
+}
+
+char *get_source_column() {
+    return source_column;
+}
+
+char *get_destination_column() {
+    return destination_column;
+}
+
+char *get_argument() {
+    return argument;
 }
--- io.h
+++ io.h
@@ -8,20 +8,35 @@
 #define WRONG_SUIT_CARD_COUNT 5
 #define WRONG_NAME_CARD_COUNT 6
 
+#define WRONG_INPUT_FORMAT 1
+#define PLAY 2
+#define QUIT_GAME 3
+#define QUIT_PROGRAM 4
+#define LOAD_FILE 5
+#define SHOW_CARDS 6
+#define SHUFFLE_SPLIT 7
+#define SHUFFLE_RANDOM 8
+#define SAVE_DECK 9
+#define MOVE_CARD 10
+
 #include "linked_list.h"
-
-/*
-char *moved_card;
-
-char *source_column;
-
-char *destination_column;
-*/
 
 linked_list *load_from_file(char *filepath);
 
 int validate_file(char *filepath);
 
 int get_user_input();
+
+void save_deck_to_file(linked_list *list, char *filepath);
+
+char *get_command();
+
+char *get_moved_card();
+
+char *get_source_column();
+
+char *get_destination_column();
+
+char *get_argument();
 
 #endif //CARD_GAME_IO_H

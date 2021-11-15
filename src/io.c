beb2bc2f75799c9e02d16e4afa11d270d1b9f027
Adam Harald Jørgensen <adamharald440@gmail.com>
2021-11-15 12:44:02 +0100

    yukon: Fixed even more weird formatting from 22de5848b51da884a04d37e81984e101afbe0aac

--- src/io.c
+++ src/io.c
@@ -404,9 +404,9 @@ void save_deck_to_file(LinkedList *list, char *filepath) {
         int bytes_written = sprintf(output_buffer, "File '%s' not created: ", filepath);
         strerror_s(output_buffer + bytes_written, MESSAGE_BUFFER_SIZE - bytes_written, errno);
 #else
-        FILE *file = fopen(filepath, "w");
-        if (!file) {
-            sprintf(output_buffer, "File '%s' not created: %s", filepath, strerror(errno));
+    FILE *file = fopen(filepath, "w");
+    if (!file) {
+        sprintf(output_buffer, "File '%s' not created: %s", filepath, strerror(errno));
 #endif
         set_message(output_buffer);
         return;
